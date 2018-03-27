b51d27e9160cfbe1c2c46d249014a6ac0c4ac045
Victoria Meng <nights1ststar@protonmail.com>
2018-03-27 23:18:03 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,12 @@
 class Backer
   attr_accessor :name
+  attr_reader :backed_projects
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
-  def back_project
+  def back_project(project)
   end
 end
